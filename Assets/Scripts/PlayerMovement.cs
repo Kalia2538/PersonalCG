07e92beab7b1b6e5bb3aa4e37298697ae12c0a5a
Kalia <Kalia2538@gmail.com>
2025-02-08 02:14:34 -0500

    finished chapters 1, 2, 5 and 6

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
+
+    private Vector2 movementValue;
+    private float lookValue;
     // Start is called before the first frame update
+    private void Awake()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        movementValue = value.Get<Vector2>() * speed;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookValue = value.Get<Vector2>().x * rotationSpeed;
+    }
+
+    void Update()
+    {
+        transform.Translate(
+            movementValue.x * Time.deltaTime,
+            0,
+            movementValue.y * Time.deltaTime);
+
+        transform.Rotate(0, lookValue * Time.deltaTime, 0);
+    }
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerShooting : MonoBehaviour
 {
     public GameObject prefab;
+    public GameObject shootPoint;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnFire(InputValue value)
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) { 
-            Instantiate(prefab);
+        if (value.isPressed)
+        {
+            GameObject clone = Instantiate(prefab);
+
+            clone.transform.position = shootPoint.transform.position;
+            clone.transform.rotation = shootPoint.transform.rotation;
         }
     }
+
+    // Update is called once per frame
+    
 }
